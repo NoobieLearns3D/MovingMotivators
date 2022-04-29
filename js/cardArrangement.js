a3ef928beedec43e1cff821d22cc0f8f6ca03718
Sanjay Ravishankar <aksam17.1237@gmail.com>
2022-04-30 02:01:47 +0530

    Drag and Drop - Two

--- js/cardArrangement.js
+++ js/cardArrangement.js
@@ -1,7 +1,11 @@
+const addedImages = [];
+var tempData = "";
 function drag(ev)
 {
     console.log(ev.target.id);
     ev.dataTransfer.setData("text",ev.target.id);
+    tempData = ev.target.id;
+    console.log(tempData)
 }
 
 const empty = document.querySelectorAll("#empty")
@@ -18,5 +22,11 @@ empty.forEach(e => {
         console.log("Dropped")
         var data = e.dataTransfer.getData("text")
         e.target.appendChild(document.getElementById(data))
+        
+        if(addedImages.indexOf(data)!==-1)
+            console.log("exists")
+        else
+            addedImages.push(tempData)
+        console.log(addedImages)
     })
 })
